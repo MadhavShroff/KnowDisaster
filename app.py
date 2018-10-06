1b6c84c3e7eb5674bc3fffbfbfa19f920c5dbe56
Madhav Shroff <madhavshroff99@gmail.com>
2018-10-06 13:16:46 +0530

    Made app simpler for testing

--- app.py
+++ app.py
@@ -1,31 +1,6 @@
-from flask import Flask, jsonify, request
-import json
-import os
+from flask import Flask
 app = Flask(__name__)
 
-with open("Database.json") as f :
-	database = json.load(f)
-
-sendMessageObject = {
-	"senderID" : "",
-	"recieverID" : "", 
-	"message" : "",
-	"messageLength": 0
-}
-
-@app.route('/')
-def homepage():
-    return "Hello, there !"
-
-@app.route('/sendMessage', methods=['POST'])
-def sendMessage():
-	recievedJson = request.json
-	try:
-		database['messages'].append(recievedJson)
-		print(recievedJson)
-	except Exception as e:
-		print(str(e))
-	return 
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def hello():
+    return "Hello World!"
