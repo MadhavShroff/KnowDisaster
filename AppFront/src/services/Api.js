ef31b1c4272e3c0f1da34e8844f46849677a18bf
Madhav Shroff <madhavshroff99@gmail.com>
2018-10-26 20:35:48 +0530

    Simplified

--- AppFront/src/services/Api.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-export default() => {
-	let port = process.env.PORT || 3000;
-	let url = `http://localhost:${port}`;
-  	return axios.create({
-    	baseURL: url,
-  });
-}
